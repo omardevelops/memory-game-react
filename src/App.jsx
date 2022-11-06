8d05fa44c81b0468f9da37804f9ac09af6ddc661
omardevelops <omarahmadp91@gmail.com>
2022-11-06 19:07:21 +0400

    implement localStorage scores

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import MainMenu from './components/MainMenu';
 import Game from './components/Game';
 import GameEndView from './components/GameEndView';
+import Button from './components/Button';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('main');
@@ -13,12 +14,38 @@ function App() {
     recordLevel: 0,
   });
 
-  const incrementScore = (type) => {
-    if (type in scores === false) {
-      throw new Error(`Score type ${type} does not exist.`);
-    }
+  // Sets LocalStorage to initial score values
+  const initializeLocalScores = () => {
+    localStorage.setItem('record', 0);
+    localStorage.setItem('recordLevel', 0);
+    console.log(localStorage);
+  };
+
+  // Fetches scores from localStorage
+  const fetchFromLocal = () => {
+    const [record, recordLevel] = [
+      localStorage.getItem('record'),
+      localStorage.getItem('recordLevel'),
+    ];
+
     const newScores = { ...scores };
-    newScores[type] = scores[type] + 1;
+    // Ensure results are integers
+    newScores.record = parseInt(record, 10);
+    newScores.recordLevel = parseInt(recordLevel, 10);
+
+    setScores(newScores);
+  };
+
+  // Updates local storage scores
+  const updateLocalScores = (newRecord, newRecordLevel) => {
+    // Initialize local storage if not init
+    localStorage.setItem('record', newRecord);
+    localStorage.setItem('recordLevel', newRecordLevel);
+  };
+
+  const incrementScore = () => {
+    const newScores = { ...scores };
+    newScores.current = scores.current + 1;
 
     setScores(newScores);
   };
@@ -33,10 +60,26 @@ function App() {
     game: <Game incrementScore={incrementScore} />,
   };
 
+  // ComponentDidMount, used to fetch scores or initialize them
+  useEffect(() => {
+    if (localStorage.length === 0) initializeLocalScores();
+    else {
+      fetchFromLocal();
+    }
+  }, []);
+
   return (
     <div className="App">
       <Header scores={scores} statusText="" />
       {pages[currentPage]}
+      <Button
+        onClick={() => {
+          updateLocalScores(1, 3);
+          fetchFromLocal();
+        }}
+      >
+        Test
+      </Button>
     </div>
   );
 }
