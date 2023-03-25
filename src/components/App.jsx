b17631af0e486ac88e77b1b91ba7f5a53cff4a97
OmarDevelops <superomar121@gmail.com>
2023-03-25 15:52:38 +0400

    reorganize file structure

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Memory Game React</h1>
+    </div>
+  );
+}
+
+export default App;
